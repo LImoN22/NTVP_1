26bac5eb2d6a7df98973352f9c545d7a3ba9457e
LImoN22 <limoncheg22@yandex.ru>
2017-01-13 05:21:59 +0700

    TEST_LAB
    Добавлена подопотная модификация лабы 1

---
+++ P4folder/GITLABS/NTVP_1/ConsoleApplication1/ClassLibrary1/CapasityMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class CapacityResCalc : MathCount
+    {
+        private double _capacityValue;
+        private double _frequencyValue;
+
+
+       /// <summary>
+        /// Конструктор для реализации метода расчёта сопротивления катушки индуктивности 
+       /// </summary>
+       /// <param name="CapacityIn"></param>
+       /// <param name="FrequencyIn"></param>
+        public CapacityResCalc(double CapacityIn, double FrequencyIn)
+        {
+            _capacityValue = CapacityIn;
+            _frequencyValue = FrequencyIn;
+
+        }
+
+        //реализация метода расчёта сопротивления конденсатора 
+        public double ResistanceCalculation
+        {
+            get
+            {
+                return (1/(2 * System.Math.PI * _frequencyValue * _capacityValue));
+            }
+        }
+    }   
+}
---
+++ P4folder/GITLABS/NTVP_1/ConsoleApplication1/ClassLibrary1/ResistanceMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// Класс резистивного метода
+    /// </summary>
+  public  class ResistanceResCalc : MathCount
+    {
+          private double _resistanceValue; 
+    
+  
+      /// <summary>
+      /// Конструктор для реализации метода расчёта сопротивления резистора 
+      /// </summary>
+      /// <param name="ResistanceIn"></param>
+      public ResistanceResCalc(double ResistanceIn)
+        {
+         _resistanceValue = ResistanceIn;
+        }
+        //метод расчёта сопротивления резистора       
+            public double ResistanceCalculation
+        {
+            get
+            {
+
+                return (_resistanceValue);
+            }
+        }
+      }
+    }
+
