89b658037797f1911153f53f3d2698a2b1d0947d
LImoN22 <limoncheg22@yandex.ru>
2017-01-13 05:29:54 +0700

    add the ntvp rev 2
    Добавил недостающую папку с проектом

---
+++ NTVP_2/NTVP_2.1/ConsoleApplication1/ClassLibrary1/CapasityMath.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class CapacityResCalc : MathCount
+    {
+        private double _capacityValue;
+        private double _frequencyValue;
+
+
+       /// <summary>
+        /// Конструктор для реализации метода расчёта сопротивления катушки индуктивности 
+       /// </summary>
+       /// <param name="CapacityIn"></param>
+       /// <param name="FrequencyIn"></param>
+        public CapacityResCalc(double CapacityIn, double FrequencyIn)
+        {
+            _capacityValue = CapacityIn;
+            _frequencyValue = FrequencyIn;
+
+        }
+
+        //метод расчёта сопротивления конденсатора 
+        public double ResistanceCalculation
+        {
+            get
+            {
+                return (1/(2 * System.Math.PI * _frequencyValue * _capacityValue));
+            }
+        }
+        public string ElementType
+        {
+            get
+            {
+                return "Capasitor resistance";
+            }
+        }
+    }   
+}
